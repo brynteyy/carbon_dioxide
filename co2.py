70602bd8537ab9a49f9f4766e6cfe04163ca8132
KevinCantrell <61003597+KevinCantrell@users.noreply.github.com>
2021-03-30 12:45:59 -0700

    fit to 2nd order polynominal

--- co2.py
+++ co2.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+def fitPly(x,c2,c1,c0):
+    y=x**2*c2+x*c1+c0
+    return y
+
 dfCarbonDioxide=pd.read_table('co2_mlo.txt',delimiter=r"\s+",skiprows=151)
 dfCarbonDioxide['date']=pd.to_datetime(dfCarbonDioxide[['year', 'month', 'day', 'hour', 'minute', 'second']])
 boolMissing=dfCarbonDioxide['value']==-999.99
@@ -14,3 +18,12 @@ dfCarbonDioxide=dfCarbonDioxide.reset_index(drop=True)
 fig, ax = plt.subplots(figsize=(12,8))
 ax.plot(dfCarbonDioxide['date'],dfCarbonDioxide['value'],'.k')
 ax.format_xdata = mdates.DateFormatter('%Y-%m-%d')
+
+startDate=min(dfCarbonDioxide['date'])
+timeElapsed=dfCarbonDioxide['date']-startDate
+daysSinceStart=timeElapsed.dt.days
+
+fitCoeffs = np.polyfit(daysSinceStart, dfCarbonDioxide['value'], 2)
+fitCO2=fitPly(daysSinceStart,fitCoeffs[0],fitCoeffs[1],fitCoeffs[2])
+ax.plot(dfCarbonDioxide['date'],fitCO2,'-r')
+
